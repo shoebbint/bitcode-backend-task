98657b9ca47fdb10b52f470d55e589b8140aa710
shoebbint <shoebbint@gmail.com>
2024-10-18 02:12:51 +0600

    validating

--- api/webhook.js
+++ api/webhook.js
@@ -1,15 +1,20 @@
+// api/webhook.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const crypto = require('crypto');
+const port = process.env.PORT || 3000;
+const webhookSecret = process.env.WEBHOOK_SECRET;
+const formBuilderApiKey = process.env.FORMBUILDER_API_KEY;
+
+require('dotenv').config();
 
 // Middleware to parse incoming JSON data
 app.use(express.json());
-const crypto = require('crypto');
 const secretKey = crypto.randomBytes(32).toString('hex');
 console.log(secretKey);
 
 // Webhook endpoint to receive POST requests
-app.post('/webhook', (req, res) => {
+app.post('/', (req, res) => {
   const formData = req.body;
   const receivedSecret = req.headers['x-webhook-secret']; // Assuming the secret is sent in a custom header
 
@@ -17,8 +22,8 @@ app.post('/webhook', (req, res) => {
 
   // Verify the secret
   if (receivedSecret !== expectedSecret) {
-      console.log('Unauthorized request. Invalid secret key.');
-      return res.status(403).send('Unauthorized');
+    console.log('Unauthorized request. Invalid secret key.');
+    return res.status(403).send('Unauthorized');
   }
 
   // Proceed with processing the form data
@@ -29,8 +34,5 @@ app.post('/webhook', (req, res) => {
   res.status(200).send('Webhook received successfully');
 });
 
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// Export the app as a serverless function
+module.exports = app;
